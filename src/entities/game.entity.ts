491b8800ab280db21ab26eb2e43b465e21c23f96
Jonas Diemer <j.diemer@babiel.com>
2024-10-25 21:56:42 +0200

    updated game entity

--- src/entities/game.entity.ts
+++ src/entities/game.entity.ts
@@ -1,11 +1,25 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Player } from './player.entity';
 
 @Entity()
 export class Game {
+
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
-  name: string;
+  timestamp: Date;
+
+  @Column()
+  team1Players: Player[];
+
+  @Column()
+  team2Players: Player[];
+
+  @Column()
+  scoreTeam1: number;
+
+  @Column()
+  scoreTeam2: number;
 
 }
